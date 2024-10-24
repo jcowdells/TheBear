7379c9ca84cc0825e42a1a07bf7f4d137ecaca55
jcowdells <jc.2408086@kedst.ac.uk>
2024-10-24 23:05:12 +0100

    improved efficiency removed Point class removed Polygon class removed Line class moved functionality all into simple functions

--- src/geometry.py
+++ src/geometry.py
@@ -1,189 +1,83 @@
 import math
-from enum import Enum, auto
 
-class Point:
-    @property
-    def x(self):
-        self.__update_rotated_point()
-        return self.__rx
+X = 0
+Y = 1
 
-    @x.setter
-    def x(self, value):
-        self.__x = value
-        self.__update_rotated_point(force=True)
+U = 0
+V = 1
 
-    @property
-    def y(self):
-        self.__update_rotated_point()
-        return self.__ry
+def line_gradient(a, b):
+    d_x = a[X] - b[X]
+    d_y = a[Y] - b[Y]
 
-    @y.setter
-    def y(self, value):
-        self.__y = value
-        self.__update_rotated_point(force=True)
+    if d_x == 0 and d_y == 0:
+        return 1, 1, 0
+    elif math.fabs(d_x) > math.fabs(d_y):
+        gradient = d_y / d_x
+        intercept = -a[Y] - gradient * a[X]
+        return gradient, 1, intercept
+    else:
+        gradient = d_x / d_y
+        intercept = -a[X] - gradient * a[Y]
+        return 1, gradient, intercept
 
-    def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
+def line_bbox(a, b):
+    min_x = min(a[X], b[X])
+    max_x = max(a[X], b[X])
+    min_y = min(a[Y], b[Y])
+    max_y = max(a[Y], b[Y])
+    return min_x, min_y, max_x, max_y
 
-        self.__rx = x
-        self.__ry = y
+def line_iter_points(a, b):
+    gx, gy, c = line_gradient(a, b)
+    min_x, min_y, max_x, max_y = line_bbox(a, b)
 
-        self.rotated = False
-        self.rotation = 0
-        self.rotation_centre = None
+    if gy == 1:
+        for x in range(min_x, max_x + 1):
+            yield line_solve_y(x, gx, c)
+    elif gx == 1:
+        for y in range(min_y, max_y + 1):
+            yield line_solve_x(y, gy, c)
 
-        self.__prev_rotation = 0
-        self.__prev_rotation_centre = None
+def line_solve_x(y, m, c):
+    return -m * y - c
 
-    def __update_rotated_point(self, force=False):
-        if self.rotation != self.__prev_rotation or self.rotation_centre != self.__prev_rotation_centre or force:
-            if self.rotation_centre is None:
-                self.__rx = self.__x
-                self.__ry = self.__y
-                return
+def line_solve_y(x, m, c):
+    return -m * x - c
 
-            c_x = self.__x - self.rotation_centre.x
-            c_y = self.__y - self.rotation_centre.y
-            r_x = c_x * math.cos(self.rotation) - c_y * math.sin(self.rotation)
-            r_y = c_x * math.sin(self.rotation) + c_y * math.cos(self.rotation)
-            self.__rx = round(r_x + self.rotation_centre.x)
-            self.__ry = round(r_y + self.rotation_centre.y)
+def triangle_signed_area(a, b, c):
+    area = (b[X] - a[X]) * (c[Y] - a[Y]) - (b[Y] - a[Y]) * (c[X] - a[X])
+    return area
 
-            self.__prev_rotation = self.rotation
-            self.__prev_rotation_centre = self.rotation_centre
+def triangle_uvw(a, b, c, p):
+    d = (b[Y] - c[Y]) * (a[X] - c[X]) + (c[X] - b[X]) * (a[Y] - c[Y])
+    u = ((b[Y] - c[Y]) * (p[X] - c[X]) + (c[X] - b[X]) * (p[Y] - c[Y])) / d
+    v = ((c[Y] - a[Y]) * (p[X] - c[X]) + (a[X] - c[X]) * (p[Y] - c[Y])) / d
+    w = 1 - u - v
+    return u, v, w
 
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
+def triangle_bbox(a, b, c):
+    min_x = min(a[X], b[X], c[X])
+    max_x = max(a[X], b[X], c[X])
+    min_y = min(a[Y], b[Y], c[Y])
+    max_y = max(a[Y], b[Y], c[Y])
+    return min_x, min_y, max_x, max_y
 
-    def __eq__(self, other):
-        if other is None:
-            return False
-        return self.x == other.x and self.y == other.y
+def triangle_contains(a, b, c, p):
+    area_a = triangle_signed_area(a, b, p)
+    area_b = triangle_signed_area(b, c, p)
+    area_c = triangle_signed_area(c, a, p)
+    return area_a >= 0 and area_b >= 0 and area_c >= 0
 
-    def __str__(self):
-        return f"({self.x}, {self.y})"
+def triangle_uv(a, b, c, uv_a, uv_b, uv_c, p):
+    uf, vf, wf = triangle_uvw(a, b, c, p)
+    u = uv_a[U] * uf + uv_b[U] * vf + uv_c[U] * wf
+    v = uv_a[V] * uf + uv_b[V] * vf + uv_c[V] * wf
+    return u, v
 
-class AccessorRegion(Enum):
-    X = auto()
-    Y = auto()
-
-class AccessorMode(Enum):
-    MIN = auto()
-    MAX = auto()
-
-class AccessorType(Enum):
-    MIN_X = AccessorMode.MIN, AccessorRegion.X
-    MAX_X = AccessorMode.MAX, AccessorRegion.X
-    MIN_Y = AccessorMode.MIN, AccessorRegion.Y
-    MAX_Y = AccessorMode.MAX, AccessorRegion.Y
-
-class Accessor(int):
-    def __new__(cls, point_a=None, point_b=None, accessor=None):
-        if point_a is None or point_b is None or accessor is None:
-            raise ValueError("Arguments 'point_a' 'point_b' and 'accessor' must not be None!")
-
-        i = int.__new__(cls, 0)
-        i.__point_a = point_a
-        i.__point_b = point_b
-        i.__accessor = accessor
-
-        i.__accessor_mode = accessor.value[0]
-        i.__accessor_region = accessor.value[1]
-
-        return i
-
-    def __int__(self):
-        if self.__accessor_region == AccessorRegion.X:
-            c_a = self.__point_a.x
-            c_b = self.__point_b.x
-        else:
-            c_a = self.__point_a.y
-            c_b = self.__point_b.y
-
-        if self.__accessor_mode == AccessorMode.MIN:
-            if c_a < c_b:
-                return c_a
-            else:
-                return c_b
-        else:
-            if c_a > c_b:
-                return c_a
-            else:
-                return c_b
-
-class GradientMode(Enum):
-    F_X = auto()
-    F_Y = auto()
-
-class Line:
-    @property
-    def __gradient(self):
-        return self.__calculate_gradient()
-
-    def __init__(self, point_a, point_b):
-        self.__point_a = point_a
-        self.__point_b = point_b
-
-        self.__min_x = Accessor(point_a=point_a, point_b=point_b, accessor=AccessorType.MIN_X)
-        self.__max_x = Accessor(point_a=point_a, point_b=point_b, accessor=AccessorType.MAX_X)
-        self.__min_y = Accessor(point_a=point_a, point_b=point_b, accessor=AccessorType.MIN_Y)
-        self.__max_y = Accessor(point_a=point_a, point_b=point_b, accessor=AccessorType.MAX_Y)
-
-        self.__cached_gradient = None
-        self.__prev_delta = None
-        self.__calculate_gradient()
-
-    def __calculate_gradient(self):
-        delta = self.__point_a - self.__point_b
-        if delta == self.__prev_delta:
-            return self.__cached_gradient
-
-        self.__prev_delta = delta
-
-        if (delta.x == 0) and (delta.y == 0):
-            self.__grad_mode = GradientMode.F_X
-            gradient = 1
-            self.__intercept = self.__point_a.y - gradient * self.__point_a.x
-        elif math.fabs(delta.x) > math.fabs(delta.y):
-            self.__grad_mode = GradientMode.F_X
-            gradient = delta.y / delta.x
-            self.__intercept = self.__point_a.y - gradient * self.__point_a.x
-        else:
-            self.__grad_mode = GradientMode.F_Y
-            gradient = delta.x / delta.y
-            self.__intercept = self.__point_a.x - gradient * self.__point_a.y
-
-        self.__cached_gradient = gradient
-        return gradient
-
-    def get_points(self):
-        if self.__grad_mode == GradientMode.F_X:
-            min_i = int(self.__min_x)
-            max_i = int(self.__max_x)
-            min_o = int(self.__min_y)
-            max_o = int(self.__max_y)
-        else:
-            min_i = int(self.__min_y)
-            max_i = int(self.__max_y)
-            min_o = int(self.__min_x)
-            max_o = int(self.__max_x)
-
-        for i in range(min_i, max_i + 1):
-            f_out = round(self.__grad_func(i))
-            if not (min_o <= f_out <= max_o):
-                continue
-            if self.__grad_mode == GradientMode.F_X:
-                yield i, f_out
-            else:
-                yield f_out, i
-
-    def __grad_func(self, i):
-        return self.__gradient * i + self.__intercept
-
-    def __str__(self):
-        if self.__grad_mode == GradientMode.F_X:
-            fstr = "y = {}x + {}"
-        else:
-            fstr = "x = {}y + {}"
-        return fstr.format(self.__gradient, self.__intercept)
+def point_rotate(p, c, angle):
+    cx = p[X] - c[X]
+    cy = p[Y] - c[Y]
+    rx = cx * math.cos(angle) - cy * math.sin(angle)
+    ry = cx * math.sin(angle) + cy * math.cos(angle)
+    return round(rx + c[X]), round(ry + c[Y])
--- src/render.py
+++ src/render.py
@@ -1,22 +1,5 @@
-import random
-import time
-
 from src.console import Console
 from src.geometry import *
-from ext.converter import get_gradient_color
-
-def triangle_signed_area(point_a, point_b, point_c):
-    return (point_b.x - point_a.x) * (point_c.y - point_a.y) - (point_b.y - point_a.y) * (point_c.x - point_a.x)
-
-def triangle_uvw(a, b, c, f):
-    signed_area = triangle_signed_area(a, b, c)
-
-    d = (b.y - c.y) * (a.x - c.x) + (c.x - b.x) * (a.y - c.y)
-    u = ((b.y - c.y) * (f.x - c.x) + (c.x - b.x) * (f.y - c.y)) / d
-    v = ((c.y - a.y) * (f.x - c.x) + (a.x - c.x) * (f.y - c.y)) / d
-    w = 1 - u - v
-
-    return u, v, w
 
 class Texture:
     def __init__(self, filepath):
@@ -38,165 +21,44 @@ class Texture:
 
                 index += 1
 
-    def get_width(self):
-        return self.__width
-
-    def get_height(self):
-        return self.__height
-
     def get_pixel(self, x, y):
         if x <= 0: x = 0
-        if x >= self.get_width(): x = self.get_width() - 1
+        if x >= self.__width: x = self.__height - 1
         if y <= 0: y = 0
-        if y >= self.get_height(): y = self.get_height() - 1
+        if y >= self.__height: y = self.__height - 1
         return self.__data[y * self.__width + x]
 
     def sample(self, x, y):
-        sx = round(x * self.get_width())
-        sy = round(y * self.get_height())
+        sx = round(x * self.__width)
+        sy = round(y * self.__height)
 
         return self.get_pixel(sx, sy)
 
-class Drawable:
-    def draw(self, console_gui):
-        pass
-
-class Polygon(Drawable):
-    def __init__(self, *args, fill="#"):
-        len_args = len(args)
-        if len_args < 3:
-            raise ValueError("A polygon must have at least 3 vertices!")
-        self.__points = args
-        self.__lines = []
-        self.__fill = fill
-        for i in range(len_args):
-            point_a = args[i]
-            if i + 1 == len_args:
-                point_b = args[0]
-            else:
-                point_b = args[i + 1]
-            self.__lines.append(Line(point_a, point_b))
-
-    def draw(self, console_gui):
-        for line in self.__lines:
-            console_gui.draw_line(line)
-
-    def rotate(self, angle, centre):
-        for point in self.__points:
-            point.rotation = angle
-            point.rotation_centre = centre
-
-class Triangle:
-    @property
-    def __points(self):
-        return self.__point_a, self.__point_b, self.__point_c
-
-    def __init__(self, point_a, point_b, point_c, uv_a, uv_b, uv_c):
-        self.__point_a = point_a
-        self.__point_b = point_b
-        self.__point_c = point_c
-        self.__uv_a = uv_a
-        self.__uv_b = uv_b
-        self.__uv_c = uv_c
-
-    def iter_x(self):
-        for point in self.__points:
-            yield point.x
-
-    def iter_y(self):
-        for point in self.__points:
-            yield point.y
-
-    def get_bbox(self):
-        min_x = None
-        max_x = None
-        for x in self.iter_x():
-            if min_x is None or x < min_x:
-                min_x = x
-            if max_x is None or x > max_x:
-                max_x = x
-
-        min_y = None
-        max_y = None
-        for y in self.iter_y():
-            if min_y is None or y < min_y:
-                min_y = y
-            if max_y is None or y > max_y:
-                max_y = y
-
-        return Point(min_x, min_y), Point(max_x, max_y)
-
-    def contains_point(self, point_p):
-        check_a = triangle_signed_area(self.__point_a, self.__point_b, point_p)
-        check_b = triangle_signed_area(self.__point_b, self.__point_c, point_p)
-        check_c = triangle_signed_area(self.__point_c, self.__point_a, point_p)
-
-        return check_a >= 0 and check_b >= 0 and check_c >= 0
-
-    def get_uv(self, point_p):
-        uf, vf, wf = triangle_uvw(*self.__points, point_p)
-
-        u = self.__uv_a[0] * uf + self.__uv_b[0] * vf + self.__uv_c[0] * wf
-        v = self.__uv_a[1] * uf + self.__uv_b[1] * vf + self.__uv_c[1] * wf
-
-        return Point(u, v)
-
-    def rotate(self, centre, angle):
-        for point in self.__points:
-            point.rotation = angle
-            point.rotation_centre = centre
-
-class Image(Drawable):
-    def __init__(self, point_a, point_b, point_c, point_d, texture):
-        self.__point_a = point_a
-        self.__point_b = point_b
-        self.__point_c = point_c
-        self.__point_d = point_d
-
-        self.__triangle_a = Triangle(point_a, point_b, point_c, (0, 0), (1, 0), (1, 1))
-        self.__triangle_b = Triangle(point_a, point_c, point_d, (0, 0), (1, 1), (0, 1))
-
-        self.__texture = texture
-
-    def draw(self, console_gui):
-        console_gui.draw_sampler(self.__triangle_a, self.__texture)
-        console_gui.draw_sampler(self.__triangle_b, self.__texture)
-
-    def rotate(self, centre, angle):
-        self.__triangle_a.rotate(centre, angle)
-        self.__triangle_b.rotate(centre, angle)
-
 class Buffer:
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         self.__buffer = bytearray(width * height)
 
     def resize(self, width, height):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         self.__buffer = bytearray(width * height)
 
     def swap(self):
-        self.__buffer = bytearray(self.__width * self.__height)
+        self.__buffer = bytearray(self.width * self.height)
 
     def get(self, x, y):
-        index = y * self.__width + x
+        index = y * self.width + x
         return self.__buffer[index]
 
     def set(self, x, y, value):
-        index = y * self.__width + x
+        index = y * self.width + x
         self.__buffer[index] = value
 
     def try_set(self, x, y, value):
-        if (0 <= x < self.__width) and (0 <= y < self.__height):
+        if (0 <= x < self.width) and (0 <= y < self.height):
             self.set(x, y, value)
-
-    def get_width(self):
-        return self.__width
-
-    def get_height(self):
-        return self.__height
 
     def as_string(self):
         string = ""
@@ -207,7 +69,7 @@ class Buffer:
             else:
                 string += chr(byte)
             count += 1
-            if count == self.__width:
+            if count == self.width:
                 string += "\n"
                 count = 0
         return string
@@ -219,70 +81,45 @@ class ConsoleGUI(Console):
     def __init__(self, width, height, x, y):
         super().__init__(width, height, x, y, 10, fg="#22BB00")
         self.buffer = Buffer(self.get_width_chars(), self.get_height_chars())
-        self.point_a = Point(10, 10)
-        self.point_b = Point(110, 10)
-        self.point_c = Point(110, 80)
-        self.point_d = Point(10, 80)
-
-        self.centre = Point(55, 45)
-
-        #self.triangle = Triangle(self.point_b, self.point_a, self.point_c)
-
         self.count = 0
-
         self.sampler = Texture("res/me.bin")
-        self.image = Image(self.point_a, self.point_b, self.point_c, self.point_d, self.sampler)
-
 
     def configure_event(self, event):
         self.buffer.resize(self.get_width_chars(), self.get_height_chars())
 
-    def draw_line(self, line, fill="#"):
-        for x, y in line.get_points():
+    def draw_line(self, a, b, fill="#"):
+        for x, y in line_iter_points(a, b):
             self.buffer.try_set(x, y, ord(fill))
 
-    def draw_image(self, image):
-        for x in range(image.get_width()):
-            for y in range(image.get_height()):
-                self.buffer.try_set(x, y, image.get_pixel(x, y))
-
-    def draw_triangle(self, triangle, fill="#"):
-        bbox = triangle.get_bbox()
-        min_x = bbox[0].x
-        min_y = bbox[0].y
-        max_x = bbox[1].x
-        max_y = bbox[1].y
-
+    def draw_triangle(self, a, b, c, fill="#"):
+        min_x, min_y, max_x, max_y = triangle_bbox(a, b, c)
         for x in range(min_x, max_x + 1):
             for y in range(min_y, max_y + 1):
-                if triangle.contains_point(Point(x, y)):
-                    self.buffer.try_set(x, y, ord(fill))
+                if triangle_contains(a, b, c, (x, y)):
+                    self.buffer.try_set(x, y, fill)
 
-    def draw_sampler(self, triangle, image):
-        bbox = triangle.get_bbox()
-        min_x = bbox[0].x
-        min_y = bbox[0].y
-        max_x = bbox[1].x
-        max_y = bbox[1].y
-
+    def draw_sampler(self, a, b, c, uv_a, uv_b, uv_c, sampler):
+        min_x, min_y, max_x, max_y = triangle_bbox(a, b, c)
         for x in range(min_x, max_x + 1):
             for y in range(min_y, max_y + 1):
-                point = Point(x, y)
-                if triangle.contains_point(Point(x, y)):
-                    uv = triangle.get_uv(point)
-                    char = image.sample(uv.x, uv.y)
-                    self.buffer.try_set(x, y, char)
+                if triangle_contains(a, b, c, (x, y)):
+                    u, v = triangle_uv(a, b, c, uv_a, uv_b, uv_c, (x, y))
+                    fill = sampler.sample(u, v)
+                    self.buffer.try_set(x, y, fill)
 
     def draw(self, drawable):
         drawable.draw(self)
 
     def main(self):
         self.count += 0.05
-        #self.point_c.x = round(self.count)
 
-        self.image.rotate(self.centre, self.count)
-        self.draw(self.image)
+        a = point_rotate((10, 10), (55, 45), self.count)
+        b = point_rotate((110, 10), (55, 45), self.count)
+        c = point_rotate((110, 80), (55, 45), self.count)
+        d = point_rotate((10, 80), (55, 45), self.count)
 
-        #self.draw_image(self.sampler)
+        self.draw_sampler(a, b, c, (0, 0), (1, 0), (1, 1), self.sampler)
+        self.draw_sampler(a, c, d, (0, 0), (1, 1), (0, 1), self.sampler)
+
         self.stdout_w(self.buffer.as_string())
         self.buffer.swap()
